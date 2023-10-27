4768fd88c41c303fa3cd5e9cb5bb5028f0c222e9
Hi Tran <thuchitran@gmail.com>
2023-10-27 22:15:52 +1100

    set up db

--- src/dao/Database.java
+++ src/dao/Database.java
@@ -1,5 +1,26 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Database {
+	private static String DB_URL = "jdbc:sqlite:analytics_hub.db";
+	private static Database instance = null;
+	
+	private Database() {}
+	
+	public static Database getInstance() {
+		if (instance == null) {
+			instance = new Database();
+		}
+		return instance;
+	}
+	
+	public Connection getConnection() throws SQLException {
+		Connection connection = null;
+		connection = DriverManager.getConnection(DB_URL);
+		return connection;
+	}
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module AnalyticsHub {
+	requires java.sql;
 }
